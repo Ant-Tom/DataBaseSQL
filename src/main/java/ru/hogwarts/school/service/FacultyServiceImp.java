59796fe63e07dd0ad4a4e68d9dc28becb8eb03d9
antontomilov <cerikwar02892@gmail.com>
2024-10-23 23:04:06 +0300

    init

---
+++ src/main/java/ru/hogwarts/school/service/FacultyServiceImp.java
@@ -0,0 +1,43 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.repository.FacultyRepository;
+
+import java.util.Optional;
+
+@Service
+public class FacultyServiceImp implements FacultyService {
+
+    private final FacultyRepository facultyRepository;
+
+    @Autowired
+    public FacultyServiceImp(FacultyRepository facultyRepository) {
+        this.facultyRepository = facultyRepository;
+    }
+
+    @Override
+    public Faculty addFaculty(Faculty faculty) {
+        return facultyRepository.save(faculty);
+    }
+
+    @Override
+    public Faculty findFaculty(long id) {
+        return facultyRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Faculty editFaculty(long id, Faculty faculty) {
+        if (facultyRepository.existsById(id)) {
+            faculty.setId(id);
+            return facultyRepository.save(faculty);
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteFaculty(long id) {
+        facultyRepository.deleteById(id);
+    }
+}
---
+++ src/main/java/ru/hogwarts/school/service/StudentServiceImp.java
@@ -0,0 +1,41 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repository.StudentRepository;
+
+@Service
+public class StudentServiceImp implements StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentServiceImp(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public Student addStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public Student findStudent(long id) {
+        return studentRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Student editStudent(long id, Student student) {
+        if (studentRepository.existsById(id)) {
+            student.setId(id);
+            return studentRepository.save(student);
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteStudent(long id) {
+        studentRepository.deleteById(id);
+    }
+}
